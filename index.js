69f6beaa50e4866989f910a8b8dd8d4f0e50e123
Leonardo <swotino@hotmail.it>
2023-01-26 19:44:00 +0100

    AWS Demo Application

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const path = require('path');
+const aws = require('aws-sdk');
+const uuid = require('uuid');
+
+
+const ACCESS_KEY = process.env.ACCESS_KEY ? process.env.ACCESS_KEY : "AKIAZBWIGR4H6DT7EUQK";
+const SECRET_KEY = process.env.SECRET_KEY ? process.env.SECRET_KEY : "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";
+const REGION = process.env.REGION ? process.env.REGION : "eu-west-1";
+
+const s3 = new aws.S3({ accessKeyId: ACCESS_KEY, secretAccessKey: SECRET_KEY, region: REGION });
+const dynamodb = new aws.DynamoDB({ accessKeyId: ACCESS_KEY, secretAccessKey: SECRET_KEY, region: REGION });
+
+async function connectedToS3() {
+  try {
+    await s3.listBuckets().promise();
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+async function connectedToDynamo() {
+  try {
+    const data = await dynamodb.listTables().promise();
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+});
+
+app.get('/api/v1/s3connected', async (req, res) => {
+  const connected = await connectedToS3();
+  res.send({connected});
+});
+
+app.get('/api/v1/dynamoconnected', async (req, res) => {
+  const connected = await connectedToDynamo();
+  res.send({ connected });
+});
+
+app.get('/api/v1/players', (req, res) => {
+
+  const data = {
+    TableName: 'demo-players'
+  }
+  dynamodb.scan(data, (err, data) => {  
+    if (err) {
+      console.log(err);
+      res.send({ success: false, data });
+    } else {
+      res.send({ success: true, data });
+    }
+  });
+});
+
+app.post('/api/v1/player', (req, res) => {
+
+  const body = req.body;
+  const data = {
+    TableName: 'demo-players',
+    Item: {
+      'id': { S: uuid.v4() },
+      'firstname': { S: body.firstname },
+      'lastname': { S: body.lastname },
+      'phone': { S: body.phone }
+    }
+  }
+  dynamodb.putItem(data, (err, data) => { 
+    if (err) {
+      console.log(err);
+      res.send({ success: false });
+    } else {
+      res.send({ success: true });
+    }
+  });
+});
+
+app.listen(80, () => {
+  console.log('Server is running on port 80');
+});
