9ab6bf2a17c7ea6e36bc0b62c188ca855e65d57d
Leonardo <swotino@hotmail.it>
2023-01-26 19:58:23 +0100

    Add the userdata

---
+++ user-data.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+// Update the system
+yum update -y
+
+// Install Nodejs
+curl -sL https://rpm.nodesource.com/setup_15.x | bash -
+yum install -y nodejs
+
+// Install Git
+yum install -y git
+
+// Create a dedicated directory for the application
+mkdir -p /var/app
+
+// Get the app from GitHub
+git clone https://github.com/swotino/aws-demo.git
+
+// Enter the app directory
+cd aws-demo
+
+// Install the app dependencies
+npm install
+
+// Start the app
+npm start
